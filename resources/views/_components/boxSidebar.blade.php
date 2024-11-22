d58639aefc55fb54e54e2357c8826105182f31b4
Danna Glz <2230061@upv.edu.mx>
2024-11-22 13:48:14 -0600

    inicio nose

---
+++ resources/views/_components/boxSidebar.blade.php
@@ -0,0 +1,5 @@
+<a href='{{ route($reference) }}' class="flex items-center p-2 text-zinc-100 rounded-lg hover:bg-red-500 group">
+    <div class="pr-3">{{ $icon }}</div>
+    <!-- <div class="appbar-text"><a href='{{ route($reference) }}'>{{ $name }}</a></div>-->
+    <span class="appbar-text text-base">{{ $name }}</span>
+</a>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.user_view', ['headers'=>[]])
+
+@section('title', 'Index')
+
+@section('content')
+    <p class="text-center">Welcome to index</p>
+@endsection
