b2db78b17661b5aa41b0880b7ff2f14da1d82e5d
cinthiaTG <2230282@upv.edu-mx>
2024-12-04 00:05:41 -0600

    autenticacion middleware

--- routes/web.php
+++ routes/web.php
@@ -6,12 +6,27 @@ use App\Http\Controllers\AsistenciaController;
 use App\Http\Controllers\EmpleadoController;
 use App\Http\Controllers\JornadaController;
 
+Route::get('/', function () {
+    return view('welcome');
+})->name('welcome');
+
+Route::get('/login', function () {
+    return view('auth.login');
+})->name('login');
+
+Route::get('/index', function () {
+    return view('index');
+})->middleware(['auth', 'verified'])->name('index');
+Route::get('/dashboard', function () {
+    return view('layouts.dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::prefix('inasistencias')->group(function () {
     Route::get('/filtro-pdf', [AsistenciaController::class, 'filtroPdfI'])->name('inasistencias.filtroPdf');
     Route::get('/generar-pdf', [AsistenciaController::class, 'generarPdfI'])->name('inasistencias.generarPdf');
     Route::post('/get-empleados', [AsistenciaController::class, 'getEmpleadosPorRolI'])->name('inasistencias.getEmpleados');
 });
+Route::middleware(['auth'])->group(function () {
 
 Route::get('asistencias/filtro-pdf', [AsistenciaController::class, 'filtroPdf'])->name('asistencias.filtroPdf');
 Route::get('asistencias/generar-pdf', [AsistenciaController::class, 'generarPdf'])->name('asistencias.generarPdf');
@@ -19,21 +34,32 @@ Route::post('/get-empleados', [AsistenciaController::class, 'getEmpleadosPorRol'
 Route::get('/inasistencias', [AsistenciaController::class, 'mostrarInasistenciasView'])->name('inasistencias.view');
 Route::post('/inasistencias', [AsistenciaController::class, 'obtenerInasistencias'])->name('inasistencias.post');
 
-
-Route::prefix('asistencias')->name('asistencias.')->group(function () {
-    Route::get('/', [AsistenciaController::class, 'index'])->name('index');
-    Route::get('/create', [AsistenciaController::class, 'create'])->name('create');
-    Route::post('/', [AsistenciaController::class, 'store'])->name('store');
-    Route::get('/{id}/edit', [AsistenciaController::class, 'edit'])->name('edit');
-    Route::put('/{id}', [AsistenciaController::class, 'update'])->name('update');
-    Route::delete('/{id}', [AsistenciaController::class, 'destroy'])->name('destroy');
 });
+
+
+Route::middleware(['auth'])->group(function () {
+
+    Route::prefix('asistencias')->name('asistencias.')->group(function () {
+        Route::get('/', [AsistenciaController::class, 'index'])->name('index');
+        Route::get('/create', [AsistenciaController::class, 'create'])->name('create');
+        Route::post('/', [AsistenciaController::class, 'store'])->name('store');
+        Route::get('/{id}/edit', [AsistenciaController::class, 'edit'])->name('edit');
+        Route::put('/{id}', [AsistenciaController::class, 'update'])->name('update');
+        Route::delete('/{id}', [AsistenciaController::class, 'destroy'])->name('destroy');
+    });
+});
+Route::middleware(['auth'])->group(function () {
+
 
 Route::prefix('empleado')->name('empleado.')->group(function () {
     Route::get('/', [EmpleadoController::class, 'index'])->name('index');
     Route::get('/create', [EmpleadoController::class, 'create'])->name('create');
     Route::post('/', [EmpleadoController::class, 'store'])->name('store'); // Importante
 });
+
+});
+Route::middleware(['auth'])->group(function () {
+
 Route::prefix('jornadas')->name('jornada.')->group(function () {
     Route::get('/', [JornadaController::class, 'index'])->name('index');
     Route::get('/create', [JornadaController::class, 'create'])->name('create');
@@ -43,19 +69,17 @@ Route::prefix('jornadas')->name('jornada.')->group(function () {
     Route::put('/{id}', [JornadaController::class, 'update'])->name('update');
     Route::delete('/{id}', [JornadaController::class, 'destroy'])->name('destroy');
 });
+});
 
 
-Route::get('/', function () {
-    return view('index');
-})->name('index');
+Route::middleware(['auth'])->group(function () {
 
 Route::get('/historial', function () {
     return view('historial_asistencias.Index');
 })->name('historial');
+});
 
-Route::get('/dashboard', function () {
-    return view('layouts.dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
